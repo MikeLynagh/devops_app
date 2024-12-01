d5e455e05d1ed83742d36d5a9497c47ba347e9c0
Mike Lynagh <mlynagh111@gmail.com>
2024-12-01 19:18:25 +0000

    added responsebody

--- src/main/java/com/example/web_app/controller/PetitionController.java
+++ src/main/java/com/example/web_app/controller/PetitionController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
